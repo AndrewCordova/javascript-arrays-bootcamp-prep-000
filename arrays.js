9483b3bb84fca1e857cf57028ada6faec361a439
Andrew Cordova <andrewcordova13@yahoo.com>
2017-09-10 18:58:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return  element.unshift
+return array.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
 }
